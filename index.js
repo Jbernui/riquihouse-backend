475d311a161deb8ebb5ca70ddc397fececb59838
Jbernui <thenoselav@gmail.com>
2026-03-13 12:03:05 -0500

    Estructura base del servidor y dependencias

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-"Chamo poto roto"
+const express = require('express');
+const cors = require('cors');
+
+// Inicializar la aplicación
+const app = express();
+
+// Middlewares (Configuraciones de seguridad y formato)
+app.use(cors()); // Permite que React se conecte sin bloqueos
+app.use(express.json()); // Permite que tu servidor entienda datos en formato JSON
+
+// Ruta de prueba (Para ver que el servidor está vivo)
+app.get('/', (req, res) => {
+    res.send('¡Servidor de Riqui House funcionando al 100%!');
+});
+
+// Definir el puerto (Usará el puerto que le dé la nube, o el 3000 en tu PC)
+const PORT = process.env.PORT || 3000;
+
+// Arrancar el servidor
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
