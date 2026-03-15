b31d2493af3800399fc7495812204a7d123ca06d
Jbernui <thenoselav@gmail.com>
2026-03-15 18:08:41 -0500

    Finalizada la lógica del sistema, backend, frontend y roles

--- controllers/produccionController.js
+++ controllers/produccionController.js
@@ -5,31 +5,25 @@ const registrarProduccion = async (req, res) => {
     
     try {
         const { id_usuario, id_producto, cantidad_producida } = req.body;
-        
-        await connection.beginTransaction(); // ⏸️ INICIAMOS TRANSACCIÓN
 
-        // 1. Guardar el registro de producción
+        await connection.beginTransaction();
         await connection.query(
             'INSERT INTO produccion (id_usuario, id_producto, cantidad_producida) VALUES (?, ?, ?)',
             [id_usuario, id_producto, cantidad_producida]
         );
 
-        // 2. ¡SUMAR el stock a la vitrina (Productos)!
         await connection.query(
             'UPDATE productos SET stock_actual = stock_actual + ? WHERE id_producto = ?',
             [cantidad_producida, id_producto]
         );
 
-        // 3. La Magia: Buscar la receta y RESTAR los insumos del almacén
         const [receta] = await connection.query(
             'SELECT id_insumo, cantidad_necesaria FROM recetas_detalle WHERE id_producto = ?',
             [id_producto]
         );
 
-        // Recorremos cada ingrediente de la receta
         for (let i = 0; i < receta.length; i++) {
             const ingrediente = receta[i];
-            // Multiplicamos (Ej: 5 tortas x 0.5kg de harina = 2.5kg a descontar)
             const cantidad_a_descontar = ingrediente.cantidad_necesaria * cantidad_producida;
 
             await connection.query(
@@ -38,14 +32,14 @@ const registrarProduccion = async (req, res) => {
             );
         }
 
-        await connection.commit(); // ✅ TODO PERFECTO, GUARDAMOS
+        await connection.commit();
         
         res.status(201).json({ 
             mensaje: '¡Producción horneada con éxito! Vitrina y almacén actualizados.' 
         });
 
     } catch (error) {
-        await connection.rollback(); // ❌ ALGO FALLÓ, REVERTIMOS TODO
+        await connection.rollback();
         console.error(error);
         res.status(500).json({ mensaje: 'Error crítico al registrar la producción' });
     } finally {
